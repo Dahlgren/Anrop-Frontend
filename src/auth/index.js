6f1089927c4c05089bca2e769250cd520d84610a
Björn Dahlgren <bjorn@dahlgren.at>
2018-07-06 21:51:56 +0200

    Update standard to version 11

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,32 +1,32 @@
 angular.module('auth', [
 ])
-.factory('AuthenticationTokenManager', function ($window) {
-  return {
-    set: function (token) {
-      $window.localStorage.token = token
-    },
-    get: function () {
-      return $window.localStorage.token
-    }
-  }
-})
-.factory('AuthenticationInterceptor', function ($q, AuthenticationTokenManager, ApiConfig) {
-  return {
-    request: function (config) {
-      if (config.url.indexOf(ApiConfig.BASE_API) === 0) {
-        var token = AuthenticationTokenManager.get()
-
-        if (token) {
-          config.headers.Authorization = 'Token token=' + token
-        }
+  .factory('AuthenticationTokenManager', function ($window) {
+    return {
+      set: function (token) {
+        $window.localStorage.token = token
+      },
+      get: function () {
+        return $window.localStorage.token
       }
-
-      return config
     }
-  }
-})
-.config(function ($httpProvider) {
-  $httpProvider.interceptors.push('AuthenticationInterceptor')
-})
+  })
+  .factory('AuthenticationInterceptor', function ($q, AuthenticationTokenManager, ApiConfig) {
+    return {
+      request: function (config) {
+        if (config.url.indexOf(ApiConfig.BASE_API) === 0) {
+          var token = AuthenticationTokenManager.get()
+
+          if (token) {
+            config.headers.Authorization = 'Token token=' + token
+          }
+        }
+
+        return config
+      }
+    }
+  })
+  .config(function ($httpProvider) {
+    $httpProvider.interceptors.push('AuthenticationInterceptor')
+  })
 
 module.exports = 'auth'
